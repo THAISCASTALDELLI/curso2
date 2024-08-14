60b4e3637e8d7b3444eaaf4d01aa8f521d0ad48a
THAIS CASTALDELLI <118453104+THAISCASTALDELLI@users.noreply.github.com>
2024-08-14 11:38:59 -0300

    tc

--- main.js
+++ main.js
@@ -7,6 +7,6 @@ const botoes = document.querySelectorALL('.perametro-senha__botao');
 botoes[0].onclik = diminuitamanho; 
 
 function diminuitamanho( ){
-
+tamanhoSenha = tamanhoSenha-1;
 }
 console.log(botoes);
