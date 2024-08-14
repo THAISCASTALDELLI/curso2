b7e87f12c96db5a4e7a678d72ebaa20f734e0c79
THAIS CASTALDELLI <118453104+THAISCASTALDELLI@users.noreply.github.com>
2024-08-14 11:36:34 -0300

    tc

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto');
 let tamanhoSenha = 12;
-numeroSenha.textContent = 5;
+numeroSenha.textContent = tamanhoSenha;
 
 const botoes = document.querySelectorALL('.perametro-senha__botao');
 
